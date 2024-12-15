c24dae070045e491891a80e16c298dd272b7343e
yeyunwen <1571374338@qq.com>
2024-12-15 16:16:37 +0800

    refactor: 移除rem适配

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import postcssPxtorem from "postcss-pxtorem";
-// import svgr from "vite-plugin-svgr";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import path from "node:path";
 
 const resolve = (dir: string) => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve("src"),
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
     },
-  },
-  plugins: [
-    react(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [resolve("src/assets/icons")],
-      // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-  ],
-  css: {
-    postcss: {
-      // plugins: [
-      //   postcssPxtorem({
-      //     rootValue: 37.5, // 设计稿宽度的1/10
-      //     propList: ["*"],
-      //   }),
-      // ],
+    plugins: [
+      react(),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve("src/assets/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
     },
-  },
+  };
 });
